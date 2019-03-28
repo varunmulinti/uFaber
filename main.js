dd4a0b05bf5b34475da5357df8d959a2bc57e167
VarunMulinti <varun9.run@gmail.com>
2019-03-28 10:57:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function openNav() {
+    $("#navContent").removeClass("sidenavContent");
+  $("#mySidenav").css('width', '374px');
+     
+     
+}
+
+function closeNav() {
+    $("#navContent").addClass("sidenavContent");
+  $("#mySidenav").css('width', '0px');
+     
+}
+
+$(document).ready(function(){
+  $(".videoImg, .videoCloseBtn").click(function () {
+        $('#videoContent, #introDiv').toggleClass('introVideoRemove');
+  }); 
+    $("#myCarousel").carousel({interval: 50000});
+});
