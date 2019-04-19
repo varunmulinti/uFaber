04d728e1760bb60d6e72fd88f60375585baf4220
VarunMulinti <varun9.run@gmail.com>
2019-04-19 18:45:11 +0530

    added new play button

--- js/main.js
+++ js/main.js
@@ -14,11 +14,25 @@ function closeNav() {
 
 $(document).ready(function(){
     
-  $(".videoImg, .videoCloseBtn").click(function () {
+  $(".imgA1, .videoCloseBtn").click(function () {
         $('#videoContent, #introDiv').toggleClass('introVideoRemove');
   }); 
     $("#myCarousel").carousel({interval: 50000});
     
-    
+    $(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
     
 });
